e0b958100e25e1749cf37a78ffdbed55f9f63b17
Sergio Campamá <sergiocampama@gmail.com>
2025-08-16 09:58:35 -0400

    fix url

--- hooks/pre_install.lua
+++ hooks/pre_install.lua
@@ -2,10 +2,8 @@ function PLUGIN:PreInstall(ctx)
   local version = ctx.version
   return {
     version = version,
-    url = "xxx",
-    sha256 = "xxx",
-    md5 = "xxx",
-    sha1 = "xxx",
-    sha512 = "xx",
+    url = "https://github.com/sergiocampama/mise-swift-format/releases/download/6.2.0/swift-format",
+    sha256 = "sha256:bc80088f39a4a7cb44c301184b5d46b2d97363bb381b57f2d75f7cb9da322710",
+    note = "Installing kaipi's swift-format version " .. version,
   }
 end
