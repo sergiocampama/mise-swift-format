5235b9cc087eeaa8ef020a0573974a7b461dca74
Sergio Campamá <sergiocampama@gmail.com>
2025-08-17 10:35:55 -0400

    fix path

--- hooks/env_keys.lua
+++ hooks/env_keys.lua
@@ -1,10 +1,9 @@
 function PLUGIN:EnvKeys(ctx)
-    --- this variable is same as ctx.sdkInfo['plugin-name'].path
   local mainPath = ctx.path
   return {
     {
       key = "PATH",
-      value = mainPath .. "/bin"
+      value = mainPath
     },
   }
 end
